3abee2650d42756f94d875909cf5012d771b6c21
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2021-07-24 19:16:16 +0430

    using filter method in js

--- script.js
+++ script.js
@@ -76,6 +76,19 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUsername = function (accs) {
+    accs.forEach(function (acc) {
+        acc.username = acc.owner
+            .toLocaleLowerCase()
+            .split(" ")
+            .map((name) => name[0])
+            .join("");
+    });
+};
+
+createUsername(accounts);
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -89,3 +102,16 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // =================================================================
+const deposits = movements.filter(function (mov) {
+    return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
