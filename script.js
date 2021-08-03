5fb172ff2a8691cbcd8e559275eed83ec675d8f4
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2021-08-03 12:21:59 +0430

    Timer edited

--- script.js
+++ script.js
@@ -249,7 +249,7 @@ const startLogOutTimer = function () {
         time--;
     };
     // set time to 5 minutes
-    let time = 27;
+    let time = 300;
     // call the timer every second
     tick();
     const timer = setInterval(tick, 1000);
